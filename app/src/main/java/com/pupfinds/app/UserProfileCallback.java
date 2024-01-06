3d135e30b4da794af2f874aa922c51ae793f069a
Tricia Rivera <triciamaerivera03@gmail.com>
2024-01-06 17:46:13 +0800

    debug

--- app/src/main/java/com/pupfinds/app/UserProfileCallback.java
+++
@@ -1,7 +0,0 @@
-package com.pupfinds.app;
-
-public interface UserProfileCallback {
-    void onUserProfileReceived(UserProfile userProfile);
-
-    void onProfileFetchError(String errorMessage);
-}
