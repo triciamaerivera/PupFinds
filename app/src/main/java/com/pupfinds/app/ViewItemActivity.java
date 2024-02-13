cac38ad881480d315b3f7064d84bacb6833b29c0
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-02-13 21:41:29 +0800

    removed unused code

--- app/src/main/java/com/pupfinds/app/ViewItemActivity.java
+++ app/src/main/java/com/pupfinds/app/ViewItemActivity.java
@@ -121,15 +121,6 @@ public class ViewItemActivity extends AppCompatActivity {
         userProgramTextView.setText(userProfile.getProgram());
         userDisplayNameTextView.setText(userProfile.getDisplayName());
     }
-
-    public static class UserProfileActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_user_profile); // Use your layout XML file
-        }
-    }
 }
 
 
